ba490b09fd86459fe4424188a86bd555a7324934
CypherC <benhammouda99@gmail.com>
2025-07-28 12:17:07 +0100

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // Use the existing Next.js + TypeScript rules
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Override rules
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
