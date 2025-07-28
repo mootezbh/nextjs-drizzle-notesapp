bfb317598aeab1f01efa115a0c1e3ee5b1ae98c0
CypherC <benhammouda99@gmail.com>
2025-07-28 13:15:49 +0100

    update

---
+++ components/Todos.tsx
@@ -0,0 +1,74 @@
+"use client";
+import { FC, useState } from "react";
+import { todoType } from "@/types/todoType";
+import Todo from "./Todo";
+import AddTodo from "./AddTodo";
+import {
+  addTodo,
+  deleteTodo,
+  editTodo,
+  toggleTodo,
+} from "@/actions/todoActions";
+import { addUser } from "@/actions/userActions";
+
+interface Props {
+  todos: todoType[];
+  user: any;
+}
+
+const Todos: FC<Props> = ({ todos, user }) => {
+  // State to manage the list of todo items
+  const [todoItems, setTodoItems] = useState<todoType[]>(todos);
+
+  // Function to create a new todo item
+  const createTodo = (text: string) => {
+    // addUser();
+    const id = new Date().getTime();
+    addTodo(id, text, user?.id);
+    setTodoItems((prev) => [
+      ...prev,
+      { id: id, text, done: false, userId: user?.id },
+    ]);
+  };
+
+  const changeTodoText = (id: number, text: string) => {
+    setTodoItems((prev) =>
+      prev.map((todo) => (todo.id === id ? { ...todo, text } : todo))
+    );
+    editTodo(id, text);
+  };
+
+  const toggleIsTodoDone = (id: number, done: boolean) => {
+    setTodoItems((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+    toggleTodo(id, done);
+  };
+
+  const deleteTodoItem = (id: number) => {
+    setTodoItems((prev) => prev.filter((todo) => todo.id !== id));
+    deleteTodo(id);
+  };
+
+  return (
+    <main className="flex mx-auto max-w-xl w-full min-h-screen flex-col items-center p-16">
+      <div className="text-5xl font-medium">To-do app</div>
+      <div className="w-full flex flex-col mt-8 gap-2">
+        {todoItems.map((todo) => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            changeTodoText={changeTodoText}
+            toggleIsTodoDone={toggleIsTodoDone}
+            deleteTodoItem={deleteTodoItem}
+          />
+        ))}
+      </div>
+      <AddTodo createTodo={createTodo} />
+    </main>
+  );
+};
+
+export default Todos;
